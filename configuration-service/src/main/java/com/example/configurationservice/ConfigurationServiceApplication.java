7fc060a23279a85008b5d02397eb07e633599e8d
Dzmitry Dziachenka <Dzmitry_Dziachenka@epam.com>
2020-12-08 23:37:04 +0300

    version 1

--- configuration-service/src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
@@ -19,21 +19,12 @@ package com.example.configurationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigurationServiceApplication {
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationServiceApplication.class, args);
